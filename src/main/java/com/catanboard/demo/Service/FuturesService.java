56861f00c51b94ea9b9af2c6febf1968286bca91
Sam Xie <muyansamxie@gmail.com>
2023-01-16 01:26:41 -0600

    regular update

--- src/main/java/com/catanboard/demo/Service/FuturesService.java
+++ src/main/java/com/catanboard/demo/Service/FuturesService.java
@@ -1,5 +1,13 @@
 package com.catanboard.demo.Service;
 
+import java.util.List;
+
+import com.catanboard.demo.POJO.Futures;
+
 public interface FuturesService{
+    Futures getFuture(Long id);
+    Futures saveFuture(Futures future);
+    void deleteFuture(Long id);
+    List<Futures> getFutures();
 
 }
--- src/main/java/com/catanboard/demo/Service/FuturesServiceImpl.java
+++ src/main/java/com/catanboard/demo/Service/FuturesServiceImpl.java
@@ -1,8 +1,26 @@
 package com.catanboard.demo.Service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.catanboard.demo.POJO.Futures;
+import com.catanboard.demo.Repository.FuturesRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Service
 public class FuturesServiceImpl implements FuturesService{
+
+    @Autowired
+    FuturesRepository futuresRepository;
+
+    @Override
+    public Futures getFuture(Long id) {
+        Optional<Futures> future = futuresRepository.findById(id);
+        return null;
+    }
     
 }
