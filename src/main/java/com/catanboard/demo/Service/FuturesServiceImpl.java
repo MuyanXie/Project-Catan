825e8ea0e6f8bfc9e13d524933abdea2a1c92106
Sam Xie <muyansamxie@gmail.com>
2023-01-22 01:41:21 -0600

    "updpate"

--- src/main/java/com/catanboard/demo/Service/FuturesServiceImpl.java
+++ src/main/java/com/catanboard/demo/Service/FuturesServiceImpl.java
@@ -1,10 +1,12 @@
 package com.catanboard.demo.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.catanboard.demo.Exceptions.FutureNotFoundException;
 import com.catanboard.demo.POJO.Futures;
 import com.catanboard.demo.Repository.FuturesRepository;
 
@@ -20,7 +22,27 @@ public class FuturesServiceImpl implements FuturesService{
     @Override
     public Futures getFuture(Long futureid) {
         Optional<Futures> future = futuresRepository.findById(futureid);
-        return null;
+        return unwrapFuture(future, futureid);
     }
-    
+
+    @Override
+    public Futures saveFuture(Futures future) {
+        return futuresRepository.save(future);
+    }
+
+    @Override
+    public void deleteFuture(Long id) {
+        futuresRepository.deleteById(id);  
+    }
+
+    @Override
+    public List<Futures> getFutures() {
+        return (List<Futures>)futuresRepository.findAll();
+    }
+
+    static Futures unwrapFuture(Optional<Futures> entity, Long id) {
+        if (entity.isPresent()) return entity.get();
+        else throw new FutureNotFoundException(id);
+    }
+
 }
