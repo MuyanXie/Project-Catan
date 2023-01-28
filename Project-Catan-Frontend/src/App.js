f50b66c8dddbb93c65590a0e39135b4aa9e6f55f
Muyan Xie <muyanxiesam@uchicago.edu>
2023-01-27 22:47:45 -0600

    update

--- Project-Catan-Frontend/src/App.js
+++ Project-Catan-Frontend/src/App.js
@@ -1,49 +1,71 @@
-
-import React, { Component } from 'react';
+import { useState } from 'react';
+// import React, { Component } from 'react';
 import './App.css';
+import MainHeader from './components/MainHeader';
 
-class App extends Component {
+function App(){
+  const [ModalisVisible, setModalisVisible] = useState(true);
 
-	constructor(props) {
-		super(props);
-		this.state = {players: []};
-	}
 
-  
-  async componentDidMount() {
-    fetch('http://localhost:8080/player/all')
-      .then(response => response.json())
-      .then(data =>  {
-        this.setState({players : data});
-    })
+  function showModalHandler(){
+    setModalisVisible(true);
   }
-  
-  render() {
 
-    return (
-
-      <div>
-        <div class= "container">
-          <table id= "table">
-            <tr>
-              <th>Player ID</th>
-              <th>Player Name</th>
-            </tr>
-              {
-                this.state.players.map((player, index) =>  
-                       <tr key={index}>
-                          <td>{player.id}</td>
-                          <td>{player.name}</td>
-                        </tr>
-                  )
-              }
-        </table>  
-              
-        </div>
-
-      </div>
-    );
+  function hideModalHandler(){
+    setModalisVisible(false);
   }
+
+  return(
+    <>
+    <MainHeader onCreatePost={showModalHandler}/>
+    </>
+  );
 }
 
 export default App;
+
+// class App extends Component {
+
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {players: []};
+// 	}
+
+  
+//   async componentDidMount() {
+//     fetch('http://localhost:8080/player/all')
+//       .then(response => response.json())
+//       .then(data =>  {
+//         this.setState({players : data});
+//     })
+//   }
+  
+//   render() {
+
+//     return (
+
+//       <div>
+//         <div class= "container">
+//           <table id= "table">
+//             <tr>
+//               <th>Player ID</th>
+//               <th>Player Name</th>
+//             </tr>
+//               {
+//                 this.state.players.map((player, index) =>  
+//                        <tr key={index}>
+//                           <td>{player.id}</td>
+//                           <td>{player.name}</td>
+//                         </tr>
+//                   )
+//               }
+//         </table>  
+              
+//         </div>
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
