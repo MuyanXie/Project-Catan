e8dc549dd6225691d72e6a5a931072ef63d56b05
Muyan Xie <apple@appledeMacBook-Pro.local>
2023-01-29 13:03:22 -0600

    update

--- Project-Catan-Frontend/src/App.js
+++ Project-Catan-Frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import RegisterForm from './components/RegistractionForm';
 import HomePage from './components/HomePage';
+import PlayerDisplay from './components/PlayerDisplay';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<RegisterForm />} />
+        <Route path="/players" element={<PlayerDisplay />}></Route>
       </Routes>
     </Router>
   )
