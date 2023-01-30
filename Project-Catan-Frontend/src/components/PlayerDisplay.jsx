f4e0c59978806da1d019d629eefaf03580a82e32
Muyan Xie <apple@appledeMacBook-Pro.local>
2023-01-30 13:18:53 -0600

    front-end dev

--- Project-Catan-Frontend/src/components/PlayerDisplay.jsx
+++ Project-Catan-Frontend/src/components/PlayerDisplay.jsx
@@ -1,41 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { Table } from "antd";
-import { createBrowserHistory as createHistory } from 'history';
+import {MdMessage} from 'react-icons/md';
+import classes from "./PlayerDisplay.module.css";
+import { useNavigate } from 'react-router-dom';
 
 const PlayerDisplay = () => {
   const [players, setPlayers] = useState([]);
-  const history = createHistory();
 
+  const navigate = useNavigate();
   useEffect(() => {
-    axios
-      .get("http://localhost:8080/player/all")
-      .then(res => setPlayers(res.data))
-      .catch(error => console.log(error));
+    fetch('http://localhost:8080/player/all')
+      .then((res) => res.json())
+      .then((data) => {
+        setPlayers(data);
+      });
   }, []);
 
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "id",
-      key: "id"
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name"
-    }
-  ];
-
   return (
+    <>
+    <header className={classes.header}>
+    <h1 className={classes.logo}>
+      <MdMessage />
+      Player Information
+    </h1>
+    <p>
+      <button className={classes.button} onClick={() => navigate('/')}>
+          Back to Home
+      </button>
+    </p>
+    </header>
+
     <div>
-      <h1>Player Information</h1>
-      <Link to="/">
-        <button>Back to Homepage</button>
-      </Link>
-      <Table dataSource={players} columns={columns} />
+      <table className = {classes.table}>
+        <thead>
+          <tr>
+            <th >ID</th>
+            <th >Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player) => (
+            <tr key={player.id}>
+              <td>{player.id}</td>
+              <td>{player.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
+    </>
   );
 };
 
